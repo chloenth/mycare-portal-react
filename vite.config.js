b7cd768edf0c0c6f956d1293e434f4afc23ff1b0
Huong <huongnguyen.nth1709@gmail.com>
2025-02-05 08:53:45 +0700

    config path resolve using alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Convert `import.meta.url` to a directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log('__filename: ', __filename);
+console.log('__dirname: ', __dirname);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+});
