7b1ca6343916480af99bdef1814c22da2bb82ad1
Huong <huongnguyen.nth1709@gmail.com>
2025-02-05 09:37:27 +0700

    remove unnecessary files and install sass

---
+++ src/components/GlobalStyles/GlobalStyles.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import './GlobalStyles.scss';
+
+const GlobalStyles = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+GlobalStyles.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default GlobalStyles;
