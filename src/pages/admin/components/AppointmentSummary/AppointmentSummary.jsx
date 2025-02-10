9d45fd3613e4997cbbfec442d8eaa802c6ba7d40
Huong <huongnguyen.nth1709@gmail.com>
2025-02-10 10:06:37 +0700

    build table UI for patient appointment section on admin dashbpard

--- src/pages/admin/components/AppointmentSummary/AppointmentSummary.jsx
+++ src/pages/admin/components/AppointmentSummary/AppointmentSummary.jsx
@@ -5,13 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChevronLeft,
   faChevronRight,
+  faSort,
 } from '@fortawesome/free-solid-svg-icons';
+import StatusLabel from '~/components/StatusLabel';
+import { Status } from '~/constants';
 
 const cx = classNames.bind(styles);
 
 const AppointmentSummary = () => {
   const [dates, setDates] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().split('T')[0],
+  );
 
   useEffect(() => {
     const generateDates = () => {
@@ -49,7 +54,12 @@ const AppointmentSummary = () => {
           {dates &&
             dates.map((date, index) => (
               <li key={index} className={cx('date-item')}>
-                <button className={cx('date-btn')}>
+                <button
+                  className={cx('date-btn', {
+                    active: date.fullDate === selectedDate,
+                  })}
+                  onClick={() => setSelectedDate(date.fullDate)}
+                >
                   {date.weekday} <span>{date.day}</span>
                 </button>
               </li>
@@ -60,6 +70,105 @@ const AppointmentSummary = () => {
           className={cx('date-control-icon')}
         />
       </div>
+
+      <table className={cx('appointment-table')}>
+        <colgroup>
+          <col style={{ width: '20%' }} />
+          <col style={{ width: '13%' }} />
+          <col style={{ width: '13%' }} />
+          <col style={{ width: '22%' }} />
+          <col style={{ width: '22%' }} />
+          <col style={{ width: '15%' }} />
+        </colgroup>
+
+        <thead>
+          <tr>
+            <th>
+              Name <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+
+            <th>
+              Date <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+
+            <th>
+              Time <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+
+            <th>
+              Doctor{' '}
+              <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+
+            <th>
+              Treatment{' '}
+              <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+
+            <th>
+              Status{' '}
+              <FontAwesomeIcon icon={faSort} className={cx('icon-sort')} />
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Caren G.Simpson</td>
+            <td>20-07-28</td>
+            <td>09:00 AM</td>
+            <td>Dr.Petra Winsburry</td>
+            <td>Routine Check-up</td>
+            <td>
+              <StatusLabel status={Status.CONFIRMED} />
+            </td>
+          </tr>
+
+          <tr>
+            <td>Edgar Warrow</td>
+            <td>20-07-28</td>
+            <td>10:30 AM</td>
+            <td>Dr.Olivia Martinez</td>
+            <td>Cardiac Consultation</td>
+            <td>
+              <StatusLabel status={Status.CONFIRMED} />
+            </td>
+          </tr>
+
+          <tr>
+            <td>Ocean Jane Lupre</td>
+            <td>20-07-28</td>
+            <td>11:00 AM</td>
+            <td>Dr.Damian Sanchez</td>
+            <td>Pediatric Check-Up</td>
+            <td>
+              <StatusLabel status={Status.PENDING} />
+            </td>
+          </tr>
+
+          <tr>
+            <td>Shane Riddick</td>
+            <td>20-07-28</td>
+            <td>01:00 PM</td>
+            <td>Dr.Chloe Harrington</td>
+            <td>Skin Allergy</td>
+            <td>
+              <StatusLabel status={Status.CANCELLED} />
+            </td>
+          </tr>
+
+          <tr>
+            <td>Queen Lawnston</td>
+            <td>20-07-28</td>
+            <td>02:30 PM</td>
+            <td>Dr.Petra Winsburry</td>
+            <td>Follow-Up Visit</td>
+            <td>
+              <StatusLabel status={Status.CONFIRMED} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
